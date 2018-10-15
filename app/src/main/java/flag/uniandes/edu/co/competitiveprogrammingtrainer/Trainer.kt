3f1c047c72b326fcb9373072dd503d078749f3f0
ncardozo <n.cardozo@uniandes.edu.co>
2018-10-14 21:22:57 -0500

    initial commit
    Empty kotlin project

---
+++ app/src/main/java/flag/uniandes/edu/co/competitiveprogrammingtrainer/Trainer.kt
@@ -0,0 +1,12 @@
+package flag.uniandes.edu.co.competitiveprogrammingtrainer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Trainer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trainer)
+    }
+}
